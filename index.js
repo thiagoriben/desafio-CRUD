f32b0b8cd24c4a7b8c5e953af5543d86075337de
Thiago Ribeiro Negreiros <thiagoriben@gmail.com>
2023-12-22 14:12:56 -0300

    index

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ function Get() {
       .then((data) => console.log(data));
   
   }
+
+  async function postData(url = '', data = {}){ 
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
+  postData('https://apigenerator.dronahq.com/api/LC9rhgnc/crud02', /* OBJETO A SER USADO */)
+  
+    .then((data) => {
+  
+      console.log(data);
+    });
